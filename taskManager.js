8adc2dda9cbcbc7e1e4816a0a31b816951e12072
XavierDM <xavier.de.moor@gmail.com>
2021-05-18 13:02:37 +0200

    added 2nd array

--- taskManager.js
+++ taskManager.js
@@ -2,7 +2,9 @@ const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
-let tasks = ["Make JS file"; "Implement JS in Website"]
+let tasks = ["Make bread", "make cake"];
+let completedTasks = [];
+
 console.log(tasks)
 const manager = () => {
     console.log("Welcome to your task manager, Press");
@@ -16,12 +18,14 @@ const manager = () => {
             console.log(`Dear coach, ${choice} is not an option within the task manager!`)
             manager()
         } else if (choice === "1") {
-            console.log(`The current tasks are: ${tasks}`)
+            let allTasks = tasks.concat(completedTasks)
+            console.log(`The current tasks are: ${allTasks}`)
             manager()
         } else if (choice === "2") {
             readline.question("Please enter the task you wish to add ", newTask => {
                 tasks.push(newTask)
                 console.log(`The task you added is ${newTask}`)
+                manager()
             })
         } else if (choice === "3") {
             console.log(`The current tasks are ${tasks}`)
@@ -29,15 +33,24 @@ const manager = () => {
                 let index = tasks.indexOf(delTask)
                 tasks.splice(index)
                 console.log(`${delTask}  will be removed from the tasks`)
-                menu()
+                manager()
             })
         } else if (choice === "4") {
             console.log(`The current tasks are ${tasks}`)
-            readline.question("Please enter the task you wish to mark as completed", compTask => {
-                let index = tasks.indexOf(compTask)
-                tasks.splice(index)
+            readline.question("Please enter the task you wish to mark as completed ", compTask => {
+                /*if value is already completed => don't show*/
+                let index1 = tasks.indexOf(`${compTask}`)
+                completedTasks.push(`${compTask}[X]`)
+                tasks.splice(index1, 1)
                 console.log(`${compTask}  will be marked as completed`)
-                menu()
+                if (completedTasks.length === 1) {
+                    console.log(`${completedTasks} is complete`)
+                } else if (completedTasks.length > 1) {
+                    console.log(`${completedTasks} are completed`)
+                } else {
+                    console.log(`There are currently no completed tasks`)
+                }
+                manager()
             })
         } else if (choice === "5") {
             console.log("Thank you for consulting the task manager!")
